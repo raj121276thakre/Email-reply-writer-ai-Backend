e548e9d9bd1a8a6219c8936a8958243921bdda8f
raj121276thakre <raj121276thakre@gmail.com>
2025-04-16 10:00:05 +0530

    setted. @CrossOrigin(origins = "*")

--- src/main/java/com/email/email_writer_ai/controller/EmailGeneratorController.java
+++ src/main/java/com/email/email_writer_ai/controller/EmailGeneratorController.java
@@ -4,14 +4,12 @@ import com.email.email_writer_ai.model.EmailRequest;
 import com.email.email_writer_ai.service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*") // used to allow use by other cross port like backend running on 8083 and frontend 5437.
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
